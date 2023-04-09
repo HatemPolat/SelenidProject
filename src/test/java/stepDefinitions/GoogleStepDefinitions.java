bde2716599b7a464e6cf6a4ad74d22baeb58b7a1
Nurcan POLAT <polathatem3653@gmail.com>
2023-04-09 04:27:41 +0300

    v.12

--- src/test/java/stepDefinitions/GoogleStepDefinitions.java
+++ src/test/java/stepDefinitions/GoogleStepDefinitions.java
@@ -1,6 +1,8 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
@@ -10,5 +12,27 @@ public class GoogleStepDefinitions {
 open(string);
 sleep(10000);
     }
+    @Given("I wait for {int} seconds")
+    public void i_wait_for_seconds(Integer int1) {
+       sleep(int1*1000);
+    }
+    @Given("I navigate back")
+    public void i_navigate_back() {
+        back();
+
+    }
+    @Given("I navigate forward")
+    public void i_navigate_forward() {
+       forward();
+    }
+    @Given("I navigate refresh the Page")
+    public void i_navigate_refresh_the_page() {
+   refresh();
+    }
+    @Then("I hold the browser open")
+    public void i_hold_the_browser_open() {
+
+    }
+
 
 }
--- src/test/resources/features/01_navigation.feature
+++ src/test/resources/features/01_navigation.feature
@@ -4,3 +4,9 @@ Feature: Navigations
 
   Scenario: navigations test
     Given I navigate to "https://www.google.com"
+    And I navigate to "http://www.amazon.com"
+    And I wait for 5 seconds
+    And I navigate back
+    And  I navigate forward
+    And I navigate refresh the Page
+    Then  I hold the browser open
